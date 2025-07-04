3ff261332a1d598793f3b3f9c92a361417781cff
Shrvin-git <shrvindavoodi@gmail.com>
2025-07-04 09:49:15 +0330

    asd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { loadFooterSite } from "./core/include.js";
 import { loadMegaMenuNavbar } from "./ui/megaMenu.js";
+import { accordionToggle } from "./ui/accordion.js";
 
 window.addEventListener('load', () => {
     loadFooterSite()
     loadMegaMenuNavbar()
+    accordionToggle()
 })
